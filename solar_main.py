6f5977e04b21125f155bc75aa63ba54efbd77620
Nurysheva Alina <alinasuper2002@mail.ru>
2020-11-13 10:15:34 +0300

    now there's a graphics printing(but not exactly what is wanted) after it's done

--- solar_main.py
+++ solar_main.py
@@ -6,6 +6,7 @@ from tkinter.filedialog import *
 from solar_vis import *
 from solar_model import *
 from solar_input import *
+from math import *
 
 perform_execution = False
 """Флаг цикличности выполнения расчёта"""
@@ -92,6 +93,34 @@ def open_file_dialog():
             raise AssertionError()
 
 
+def print_graphics(f):
+    root = Tk()
+    canv = Canvas(root, width=1000, height=1000, bg="white")
+    canv.create_line(500, 1000, 500, 0, width=2, arrow=LAST)
+    canv.create_line(0, 500, 1000, 500, width=2, arrow=LAST)
+
+    First_x = -500;
+
+    for i in range(16000):
+        if (i % 800 == 0):
+            k = First_x + (1 / 16) * i
+            canv.create_line(k + 500, -3 + 500, k + 500, 3 + 500, width=0.5, fill='black')
+            canv.create_text(k + 515, -10 + 500, text=str(k), fill="purple", font=("Helvectica", "10"))
+            if (k != 0):
+                canv.create_line(-3 + 500, k + 500, 3 + 500, k + 500, width=0.5, fill='black')
+                canv.create_text(20 + 500, k + 500, text=str(k), fill="purple", font=("Helvectica", "10"))
+        try:
+            x = First_x + (1 / 16) * i
+            new_f = f.replace('x', str(x))
+            y = -eval(new_f) + 500
+            x += 500
+            canv.create_oval(x, y, x + 1, y + 1, fill='black')
+        except:
+            pass
+    canv.pack()
+    root.mainloop()
+
+
 def save_file_dialog():
     """Открывает диалоговое окно выбора имени файла и вызывает
     функцию считывания параметров системы небесных тел из данного файла.
@@ -152,3 +181,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    print_graphics('x')
